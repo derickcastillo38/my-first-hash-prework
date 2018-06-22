fe2860e76fa36142fb4097c1527be97d125925fa
derickcastillo38 <9derick@gmail.com>
2018-06-22 03:51:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  derick = Hash.new
-  derick[a] = 1
+  your_hash = {'a'=>1, 'b'=>2}
 
 end
 
